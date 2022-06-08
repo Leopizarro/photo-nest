57ebb6964a28140d822c06edba04dd8a1b2a31fb
Leonardo Pizarro <leonardo.pizarro@mail.udp.cl>
2022-06-08 17:41:26 -0400

    environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: 'AIzaSyCsWTtOmtV63_Jcn7XjMqJX-ArJvYdeqfY',
+        authDomain: 'photo-nest-9c6f9.firebaseapp.com',
+        projectId: 'photo-nest-9c6f9',
+        storageBucket: 'photo-nest-9c6f9.appspot.com',
+        messagingSenderId: '284201921603',
+        appId: '1:284201921603:web:2e685a9d65b423f376b8f6'
+    }
+};
